af85268f718ec91c62b1f27fd4640a22c7f6b044
dyonghan <dongyonghan@huawei.com>
2020-05-18 09:45:17 +0800

    !1 Initial version Initial version

--- README.en.md
+++ README.en.md
@@ -1,36 +1,7 @@
 # course
 
-#### Description
-{**When you're done, you can delete the content in this README and update the file with details for others getting started with your repository**}
+The experimental guidance based on the MindSpore open source deep learning framework. It is only used for teaching or training purposes.
 
-#### Software Architecture
-Software architecture description
+Part of the content comes from the open source community, internet or third party. If something violates your rights, please leave a message via issue or submit a pull request.
 
-#### Installation
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### Instructions
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### Contribution
-
-1.  Fork the repository
-2.  Create Feat_xxx branch
-3.  Commit your code
-4.  Create Pull Request
-
-
-#### Gitee Feature
-
-1.  You can use Readme\_XXX.md to support different languages, such as Readme\_en.md, Readme\_zh.md
-2.  Gitee blog [blog.gitee.com](https://blog.gitee.com)
-3.  Explore open source project [https://gitee.com/explore](https://gitee.com/explore)
-4.  The most valuable open source project [GVP](https://gitee.com/gvp)
-5.  The manual of Gitee [https://gitee.com/help](https://gitee.com/help)
-6.  The most popular members  [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+Please go to [MindSpore Open Source Community] (https://www.mindspore.cn/) for more videos and documentation tutorials.
---
+++ experiment_2/main.py
@@ -0,0 +1,142 @@
+# Save and load model
+
+import os
+# os.environ['DEVICE_ID'] = '0'
+# Log level includes 3(ERROR), 2(WARNING), 1(INFO), 0(DEBUG).
+os.environ['GLOG_v'] = '2'
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import mindspore as ms
+import mindspore.context as context
+import mindspore.dataset.transforms.c_transforms as C
+import mindspore.dataset.transforms.vision.c_transforms as CV
+
+from mindspore.dataset.transforms.vision import Inter
+from mindspore import nn, Tensor
+from mindspore.train import Model
+from mindspore.train.callback import ModelCheckpoint, CheckpointConfig, LossMonitor
+from mindspore.train.serialization import load_checkpoint, load_param_into_net
+
+import logging; logging.getLogger('matplotlib.font_manager').disabled = True
+
+context.set_context(mode=context.GRAPH_MODE, device_target='Ascend')
+
+DATA_DIR_TRAIN = "MNIST/train" # 训练集信息
+DATA_DIR_TEST = "MNIST/test" # 测试集信息
+
+
+def create_dataset(training=True, num_epoch=1, batch_size=32, resize=(32, 32),
+                   rescale=1/(255*0.3081), shift=-0.1307/0.3081, buffer_size=64):
+    ds = ms.dataset.MnistDataset(DATA_DIR_TRAIN if training else DATA_DIR_TEST)
+    
+    # define map operations
+    resize_op = CV.Resize(resize)
+    rescale_op = CV.Rescale(rescale, shift)
+    hwc2chw_op = CV.HWC2CHW()
+    
+    # apply map operations on images
+    ds = ds.map(input_columns="image", operations=[resize_op, rescale_op, hwc2chw_op])
+    ds = ds.map(input_columns="label", operations=C.TypeCast(ms.int32))
+    
+    ds = ds.shuffle(buffer_size=buffer_size)
+    ds = ds.batch(batch_size, drop_remainder=True)
+    ds = ds.repeat(num_epoch)
+    
+    return ds
+
+
+class LeNet(nn.Cell):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(1, 6, 5, stride=1, pad_mode='valid')
+        self.conv2 = nn.Conv2d(6, 16, 5, stride=1, pad_mode='valid')
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Dense(400, 120)
+        self.fc2 = nn.Dense(120, 84)
+        self.fc3 = nn.Dense(84, 10)
+    
+    def construct(self, input_x):
+        output = self.conv1(input_x)
+        output = self.relu(output)
+        output = self.pool(output)
+        output = self.conv2(output)
+        output = self.relu(output)
+        output = self.pool(output)
+        output = self.flatten(output)
+        output = self.fc1(output)
+        output = self.fc2(output)
+        output = self.fc3(output)
+        
+        return output
+
+    
+LOOP_SINK = context.get_context('enable_loop_sink')
+
+def test_train(lr=0.01, momentum=0.9, num_epoch=2, check_point_name="b_lenet"):
+    ds_train = create_dataset(num_epoch=num_epoch)
+    ds_eval = create_dataset(training=False)
+    steps_per_epoch = ds_train.get_dataset_size()
+    
+    net = LeNet()
+    loss = nn.loss.SoftmaxCrossEntropyWithLogits(is_grad=False, sparse=True, reduction='mean')
+    opt = nn.Momentum(net.trainable_params(), lr, momentum)
+    
+    ckpt_cfg = CheckpointConfig(save_checkpoint_steps=steps_per_epoch, keep_checkpoint_max=5)
+    ckpt_cb = ModelCheckpoint(prefix=check_point_name, config=ckpt_cfg)
+    loss_cb = LossMonitor(per_print_times=1 if LOOP_SINK else steps_per_epoch)
+    
+    model = Model(net, loss, opt, metrics={'acc', 'loss'})
+    model.train(num_epoch, ds_train, callbacks=[ckpt_cb, loss_cb], dataset_sink_mode=True)
+    metrics = model.eval(ds_eval)
+    print('Metrics:', metrics)
+
+
+CKPT = 'b_lenet-2_1875.ckpt'
+
+def resume_train(lr=0.001, momentum=0.9, num_epoch=2, ckpt_name="b_lenet"):
+    ds_train = create_dataset(num_epoch=num_epoch)
+    ds_eval = create_dataset(training=False)
+    steps_per_epoch = ds_train.get_dataset_size()
+    
+    net = LeNet()
+    loss = nn.loss.SoftmaxCrossEntropyWithLogits(is_grad=False, sparse=True, reduction='mean')
+    opt = nn.Momentum(net.trainable_params(), lr, momentum)
+    
+    param_dict = load_checkpoint(CKPT)
+    load_param_into_net(net, param_dict)
+    load_param_into_net(opt, param_dict)
+    
+    ckpt_cfg = CheckpointConfig(save_checkpoint_steps=steps_per_epoch, keep_checkpoint_max=5)
+    ckpt_cb = ModelCheckpoint(prefix=ckpt_name, config=ckpt_cfg)
+    loss_cb = LossMonitor(per_print_times=1 if LOOP_SINK else steps_per_epoch)
+    
+    model = Model(net, loss, opt, metrics={'acc', 'loss'})
+    model.train(num_epoch, ds_train, callbacks=[ckpt_cb, loss_cb])
+    
+    metrics = model.eval(ds_eval)
+    print('Metrics:', metrics)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_url', required=True, default=None, help='Location of data.')
+    parser.add_argument('--train_url', required=True, default=None, help='Location of training outputs.')
+    parser.add_argument('--num_epochs', type=int, default=1, help='Number of training epochs.')
+    args, unknown = parser.parse_known_args()
+
+    import moxing as mox
+    mox.file.copy_parallel(src_url=args.data_url, dst_url='MNIST/')
+
+    os.system('rm -f *.ckpt *.ir *.meta') # 清理旧的运行文件
+
+    test_train()
+    print('\n'.join(sorted([x for x in os.listdir('.') if x.startswith('b_lenet')])))
+
+    resume_train()
+    print('\n'.join(sorted([x for x in os.listdir('.') if x.startswith('b_lenet')])))
+    
